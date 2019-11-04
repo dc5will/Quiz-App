bb6110d9c4a1ba6f8ba010ec2352047b89ffa0c9
William Wong <williamwdev@gmail.com>
2019-11-03 21:53:58 -0800

    set up very basic question and answer bank

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var quizQuestions = [
+  {
+    question: "Which one of these activities would you enjoy the most ? ",
+    answers: [
+      {
+        type: "Tanjiro",
+        content: "Headbutting your enemies"
+      },
+
+      {
+        type: "Inosuke",
+        content: "Try to defeat everyone at everything"
+      },
+      {
+        type: "Zenitsu",
+        content: "Chase after anyone you think is attractive"
+      }
+    ]
+  },
+  {
+    question: "Which one of these colors do you like the most ? ",
+    answers: [
+      {
+        type: "Inoksuke",
+        content: "Green"
+      },
+
+      {
+        type: "Tanjiro",
+        content: "Red"
+      },
+      {
+        type: "Zenitsu",
+        content: "Yellow"
+      }
+    ]
+  },
+  {
+    question: "Who in your life do you value the most ? ",
+    answers: [
+      {
+        type: "Tanjiro",
+        content: "Your little sister"
+      },
+
+      {
+        type: "Inosuke",
+        content: "No one"
+      },
+      {
+        type: "Zenitsu",
+        content: "Grandpa"
+      }
+    ]
+  },
+  {
+    question: "What trait best describes you ? ",
+    answers: [
+      {
+        type: "Tanjiro",
+        content: "Very caring and try to see the best in people"
+      },
+
+      {
+        type: "Inosuke",
+        content: "Stubborn and extremely competitive"
+      },
+      {
+        type: "Zenitsu",
+        content: "Scared of everything, but easily falls in love"
+      }
+    ]
+  },
+];
+
+export default quizQuestions;
